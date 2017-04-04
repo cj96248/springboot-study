e8ab8a8e47c4b8a833d958dfa4af6bc34c1cc0f5
cj96248 <253037585@qq.com>
2017-04-04 22:19:46 +0800

    refactor the project

--- src/main/java/com/example/training/MyRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.training;
-
-import java.io.Serializable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyRespository extends JpaRepository<People, Serializable>{
-
-}
