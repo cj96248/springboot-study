3fdcfca1670f5e2dde958657d53557250ec811f8
cj96248 <cj96248@sina.com>
2019-01-29 21:12:41 +0800

    项目初始化

--- src/main/java/com/chao/training/ExampleApplication.java
+++ src/main/java/com/chao/training/ExampleApplication.java
@@ -1,13 +1,12 @@
-package com.example.training;
+package com.chao.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Begin ------");
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 	}
 }
--- src/main/java/com/example/training/service/MyService.java
+++
@@ -1,5 +0,0 @@
-package com.example.training.service;
-
-public class MyService {
-
-}
