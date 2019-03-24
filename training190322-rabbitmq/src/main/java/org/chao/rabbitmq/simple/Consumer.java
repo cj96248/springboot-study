8ec7da983c7d665040c02e79b230b4af56f4b5ae
cj96248 <cj96248@sina.com>
2019-03-24 22:00:02 +0800

    示例升级

--- training190322-rabbitmq/src/main/java/org/chao/rabbitmq/simple/Consumer.java
+++ training190322-rabbitmq/src/main/java/org/chao/rabbitmq/simple/Consumer.java
@@ -1,9 +1,14 @@
 package org.chao.rabbitmq.simple;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
 
 public class Consumer {
 
@@ -11,7 +16,7 @@ public class Consumer {
 		// 创建一个 ConnectionFactory
 		ConnectionFactory factory = new ConnectionFactory();
 		
-		// 设置RabbitMQ参数
+		// 设置RabbitMQ连接参数
 		factory.setHost("localhost");
 		factory.setPort(5672);
 		factory.setVirtualHost("/");
@@ -19,19 +24,30 @@ public class Consumer {
 		// 获取一个连接
 		Connection connection = factory.newConnection();
 		
-		// 创建Channel
+		// 创建信道Channel
 		Channel channel = connection.createChannel();
 		
 		// 声明一个队列
 		String queueName = "test";
-		channel.queueDeclare(queueName, true, false, false, null);
 
 		// 创建消费者
-		DefaultConsumer consumer = new DefaultConsumer(channel);
-		
-		channel.basicConsume(queueName, true, consumer);
+		DefaultConsumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+					throws IOException {
+				System.out.println("Received message :" + new String(body));
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				channel.basicAck(envelope.getDeliveryTag(), false);
+			}
+		};
 		
 		// 获取消息
+		channel.basicConsume(queueName, true, consumer);
+		
 		// 释放资源，关闭连接
 		channel.close();
 		connection.close();
--- training190322-rabbitmq/src/main/java/org/chao/rabbitmq/simple/Producer.java
+++ training190322-rabbitmq/src/main/java/org/chao/rabbitmq/simple/Producer.java
@@ -1,11 +1,16 @@
 package org.chao.rabbitmq.simple;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Producer {
 	public static void main(String[] args) throws Exception {
+		
+		String exchangeName = "exchange_test";
+		String queueName = "test";
+		
 		// 创建一个 ConnectionFactory
 		ConnectionFactory factory = new ConnectionFactory();
 		
@@ -20,9 +25,16 @@ public class Producer {
 		// 创建Channel
 		Channel channel = connection.createChannel();
 		
+		// 创建一个"direct"类型、非排他、非自动删除的交换机
+		channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT, true, false, null);
+		
+		// 创建一个持久化、非排他的、非自动删除的队列
+		channel.queueDeclare(queueName, true, false, false, null);
+		
+		channel.queueBind(queueName, exchangeName, "key_test");
 		// 发送数据
 		String message = "Hello world!";
-		channel.basicPublish("", "test", null, message.getBytes());
+		channel.basicPublish(exchangeName, "key_test", null, message.getBytes());
 		
 		// 释放资源，关闭连接
 		channel.close();
